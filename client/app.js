c943ca64d6ce8e0d99d00430eba66ee02e9ae2f7
Parashuram <code@r.nparashuram.com>
2013-08-10 15:45:55 -0700

    Added maps interface

--- client/app.js
+++ client/app.js
@@ -1,22 +1,49 @@
-var handle, server = 'http://127.0.0.1:32123/';
+var handle;
+var server = 'http://' + document.location.hostname + ':32123/server/';
+
+navigator.geolocation.watchPosition(geo_success, geo_error, {enableHighAccuracy: true,maximumAge: 30000,timeout: 27000});
 
 $('.start').click(function(){
 	$.getJSON(server + 'start?callback=?', function(data) {
-		$('.stop').toggleClass('hide');
 		$('.start').toggleClass('hide');
-
+		$('.status').toggleClass('hide');
 		handle = window.setInterval(function(){
-			$.getJSON(server + 'status?callback=?', function(data){
+			$.getJSON(server + 'status?callback=?&time=', function(data){
+				console.log(data);
 				if (data && data.status && data.status === 'complete'){
 					window.clearInterval(handle);
+					$('.parking-searching').addClass('hide');
+					$('.parking-found').removeClass('hide');
 				}
 			});
 		}, 1000);
 	});
 });
 
-$('.stop').click(function(){
+$('#recallDrone').on('click',function(){
 	$.getJSON(server + 'stop?callback=?', function(data) {
 		window.clearInterval(handle);
+		$('#recallDrone').hide();
+		$('#recalling').removeClass('hide');
 	});
 });
+
+function geo_success(pos){
+	var latlong = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+	var mapOptions = {
+		zoom: 17,
+		center: latlong,
+		mapTypeId: google.maps.MapTypeId.ROAD
+	}
+	var map = new google.maps.Map(document.getElementById("maps"), mapOptions);
+	var marker = new google.maps.Marker({
+	    position: latlong,
+	    title:"Drone Location"
+	});
+
+	marker.setMap(map);
+}
+
+function geo_error(){
+
+}
--- server/drone.js
+++ server/drone.js
@@ -9,6 +9,15 @@ exports.stop = function(cb){
 	cb({status: true})
 }
 
+var count = 0;
 exports.status = function(cb){
-	cb({status: 'complete'});
+	console.log(count);
+	count++;
+	if (count > 10){
+		cb({status: 'complete'});
+		count = 0;
+	} else {
+		cb({status: 'searching', count: count});
+	}
+	
 }
